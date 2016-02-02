17067ee65e8b53b94d7fb08e6c185da705c1c864
johannesvaltonen <johannes.valtonen@affecto.com>
2016-02-02 15:40:05 +0200

    Added an interface for domain event brokers. Added a method for publishing a single domain event.

---
+++ Domain.Tests/TestHelpers/AnotherTestDomainEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Affecto.Patterns.Domain.Tests.TestHelpers
+{
+    public class AnotherTestDomainEvent : DomainEvent
+    {
+        public AnotherTestDomainEvent(Guid entityId, long entityVersion)
+            : base(entityId, entityVersion)
+        {
+        }
+
+        public AnotherTestDomainEvent(Guid entityId)
+            : base(entityId)
+        {
+        }
+    }
+}
---
+++ Domain/IDomainEventBroker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Affecto.Patterns.Domain
+{
+    /// <summary>
+    /// A broker that publishes domain events to domain event handlers.
+    /// </summary>
+    public interface IDomainEventBroker
+    {
+        /// <summary>
+        /// Publishes an event to all event handlers that are registered to it.
+        /// </summary>
+        /// <param name="event">Domain event.</param>
+        void PublishEvent(IDomainEvent @event);
+
+        /// <summary>
+        /// Publishes events to all event handlers that are registered to the event.
+        /// </summary>
+        /// <param name="events">Domain events.</param>
+        void PublishEvents(IEnumerable<IDomainEvent> events);
+    }
+}
