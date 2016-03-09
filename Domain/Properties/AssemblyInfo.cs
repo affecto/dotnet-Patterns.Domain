20ff3c767e6f302bd9751840520107a320130901
johannesvaltonen <johannes.valtonen@affecto.com>
2016-03-09 14:48:38 +0200

    Release version 4.0.0.

--- Domain/Properties/AssemblyInfo.cs
+++ Domain/Properties/AssemblyInfo.cs
@@ -8,4 +8,4 @@
 [assembly: AssemblyFileVersion("4.0.0.0")]
 
 // This version is used by NuGet:
-[assembly: AssemblyInformationalVersion("4.0.0-prerelease01")]
+[assembly: AssemblyInformationalVersion("4.0.0")]
